4b62425c366bb3f748ca3efa057b2d29a7ae0413
Muh. Fauzan Arif <ariffauzan744@gmail.com>
2017-11-05 10:05:13 +0700

    Halo Dunia
    Kode diubah untuk menampilkan Halo Dunia

--- src/appsensi/Appsensi.java
+++ src/appsensi/Appsensi.java
@@ -15,7 +15,7 @@ public class Appsensi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Halo Dunia!");
     }
     
 }
